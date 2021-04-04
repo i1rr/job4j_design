fc4fd2efce97a551a42c8afb658447b88c99392b
i1rr <i1r@tuta.io>
2021-04-04 12:57:12 +1000

    Even numbers iterator

---
+++ src/main/java/ru/job4j/it/EvenIt.java
@@ -0,0 +1,36 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIt implements Iterator<Integer> {
+
+    private final int[] data;
+    private int pointer;
+
+    public EvenIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean check;
+        for (int i = pointer; i < data.length; i++) {
+           check = data[i] % 2 == 0;
+           if (check) {
+               break;
+           } else {
+               ++pointer;
+           }
+        }
+        return pointer < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[pointer++];
+    }
+}
