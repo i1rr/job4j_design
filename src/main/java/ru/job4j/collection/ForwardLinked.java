b122453adb991662ff5b490d986785a32e1dd332
i1rr <i1r@tuta.io>
2021-05-09 21:52:18 +1000

    Stack container based on ForwardLinked

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -19,6 +19,14 @@ public class ForwardLinked<T> implements Iterable<T> {
         tail.next = node;
     }
 
+    public void addFirst(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head != null) {
+            node.next = head;
+        }
+        head = node;
+    }
+
     public T deleteFirst() {
         if (head != null) {
             Node<T> temp = head;
