0fad0a39ecb53f986d9e9a97262d506ab5a63dc3
i1rr <i1r@tuta.io>
2021-05-09 21:18:56 +1000

    Wiping HEAD link in single linked list.

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,63 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public T deleteFirst() {
+        if (head != null) {
+            Node<T> temp = head;
+            head = head.next;
+            temp.next = null;
+            return temp.value;
+        }
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private final T value;
+        private Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
