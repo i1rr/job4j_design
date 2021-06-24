79ce0c7052f6ec4b5fbc42dcd6fc54d220021a78
i1rr <i1r@tuta.io>
2021-06-24 20:31:50 +1000

    0.4. BufferedReader. upd - bug fixes

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -6,12 +6,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
+
+    public static List<String> filter(String file) {
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            rsl = in.lines().filter(filter -> filter.contains("404"))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String str : log) {
+                out.println(str + System.lineSeparator());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Deprecated
     public static List<String> filter(ArrayList<String> list, String filter) {
         return list.stream()
                 .filter(filterBy -> filterBy.contains(filter))
                 .collect(Collectors.toList());
     }
 
+    @Deprecated
     public static ArrayList<String> getFileToArrayList(String file) {
         ArrayList<String> lines = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
@@ -24,25 +51,11 @@ public class LogFilter {
         return lines;
     }
 
-    public static void save(List<String> log, String file) {
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(file)
-                ))) {
-            for (String str : log) {
-                out.write(str + System.lineSeparator());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        ArrayList<String> list = getFileToArrayList("log.txt");
-        List<String> log = filter(list, "404");
-        for (String string : log) {
+        List<String> list = filter("log.txt");
+        for (String string : list) {
             System.out.println(string);
         }
-        save(log, "404.txt");
+        save(list, "404.txt");
     }
 }
