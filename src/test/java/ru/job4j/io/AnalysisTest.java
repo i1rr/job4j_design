8dd5885e9a7ca25501bf6c4e1854df551f2b4b29
i1rr <i1r@tuta.io>
2021-06-28 20:37:46 +1000

    Server status analysis. v3 + Temporary folder  - code 400 || 500 -> server down  - code 200 || 300 -> server up

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -1,13 +1,38 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
-
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AnalysisTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenTempReport() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n"
+                    + "500 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01\n"
+                    + "500 11:01:02\n"
+                    + "200 11:02:02");
+        }
+        Analysis log = new Analysis();
+        log.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        List<String> rsl = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
+        assertThat(log.load(target.getAbsolutePath()), is(rsl));
+    }
 
     @Test
     public void whenReport1is2reboot() {
