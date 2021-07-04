e596e65150601a391efff381ff92f1dec302c576
i1rr <i1r@tuta.io>
2021-07-04 19:13:44 +1000

    5.1 Named arguments.

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (values.get(key) == null) {
+            throw new IllegalArgumentException();
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String str : args) {
+            if (str.startsWith("-")) {
+                String[] keyValue = str.substring(1).trim().split("=");
+                if (keyValue.length < 2) {
+                    throw new IllegalArgumentException();
+                }
+                values.put(keyValue[0], keyValue[1]);
+            }
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"
+        });
+        System.out.println(zip.get("out"));
+    }
+}
