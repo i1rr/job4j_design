500a84e67ccee8a534b4cabe1e8c34ff2cf5031a
i1rr <i1r@tuta.io>
2021-07-10 21:50:57 +1000

    1. Bot (EchoServer)

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,22 +5,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                    for (String str = in.readLine();
-                         str != null && !str.isEmpty();
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    for (String str = in.readLine(); str != null && !str.isEmpty();
                          str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("msg=Bye")) {
-                            server.close();
+                        if (str.contains("msg=")) {
+                            String msg = str.substring(10, str.length() - 9);
+                            if (msg.equals("Hello")) {
+                                out.write("Hello\n".getBytes());
+                            } else if (msg.equals("Exit")) {
+                                server.close();
+                            } else {
+                                out.write("What?\n".getBytes());
+                            }
                         }
                     }
+                    out.flush();
                 }
             }
         }
