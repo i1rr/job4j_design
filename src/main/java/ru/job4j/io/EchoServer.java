ace70cf4c6b7860643cb317558a62287a2e60bbc
i1rr <i1r@tuta.io>
2021-07-11 17:41:03 +1000

    4. Slf4j - exception to log.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,8 +4,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -30,6 +35,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (Exception e) {
+            LOG.error("EchoServer exception in log", e);
         }
     }
 }
