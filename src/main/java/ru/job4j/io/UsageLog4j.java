24bbf314ed08122cdfefc8ea206f0e875c7f5d02
i1rr <i1r@tuta.io>
2021-07-11 16:37:51 +1000

    3. Slf4j lib (logger layer)

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,26 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        boolean first = true;
+        byte second = Byte.MAX_VALUE;
+        short third = 32767;
+        int fifth = Integer.MIN_VALUE;
+        long sixth = 9223372036854775807L;
+        float seventh = 1.0f;
+        double eighth = 2.0;
+
+        char j = 'j';
+        char a = 'a';
+        char v = 'v';
+        char d = 'd';
+        char e = 'e';
+        char t = 't';
+        char k = 'k';
+        char o = 'o';
+
+        LOG.debug("\nThis is a reeeeeal debug baby:\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n",
+                first, second, third, fifth, sixth, seventh, eighth);
+        LOG.debug("Короче, {}{}{} {}{}{}{} {}{}{}{}{}",
+                e, t, o, j, a, v, a, d, e, t, k, a);
     }
 }
