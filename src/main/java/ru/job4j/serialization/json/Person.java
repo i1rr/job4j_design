ff5a17b5b129afacd851a9f54b611da40482bb57
i1rr <i1r@tuta.io>
2021-07-18 00:46:30 +1000

    5. JsonObject. JSON to POJO.

--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -2,8 +2,12 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Person {
     private final boolean sex;
@@ -18,6 +22,14 @@ public class Person {
         this.statuses = statuses;
     }
 
+    private int getAge() {
+        return age;
+    }
+
+    private boolean isSex() {
+        return sex;
+    }
+
     @Override
     public String toString() {
         return "Person{"
@@ -29,13 +41,40 @@ public class Person {
     }
 
     public static void main(String[] args) {
+
+        // JSONObject из json-строки строки
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        // JSONArray из ArrayList
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        // JSONObject напрямую методом put
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.isSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        // Выведем результат в консоль
+        System.out.println(jsonObject.toString());
+
+        // Преобразуем объект person в json-строку
+        System.out.println(new JSONObject(person).toString());
+    }
+}
+
+        /*
         final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
 
-        /* Преобразуем объект person в json-строку. */
+        //Преобразуем объект person в json-строку
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(person));
 
-        /* Модифицируем json-строку */
+        //Модифицируем json-строку
         final String personJson =
                 "{"
                         + "\"sex\":false,"
@@ -49,5 +88,4 @@ public class Person {
                         + "}";
         final Person personMod = gson.fromJson(personJson, Person.class);
         System.out.println(personMod);
-    }
-}
+         */
--- src/main/java/ru/job4j/serialization/json/Spell.java
+++ src/main/java/ru/job4j/serialization/json/Spell.java
@@ -2,8 +2,12 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Spell {
     private final boolean castable;
@@ -29,8 +33,27 @@ public class Spell {
     }
 
     public static void main(String[] args) {
+        //JSONObject from json-String
+        JSONObject jsonFBall = new JSONObject("{\"damage\":\"+200\"}");
+
+        //JSON Array out of ArrayList
+        List<String> list = new ArrayList<>();
+        list.add("18.02.2021::22:18");
+        list.add("18.02.2021::22:22");
+        JSONArray jsonLog = new JSONArray(list);
+
+        //JSONObject using put
         final Spell spell = new Spell(true, 30, new Fireball(200),
-                "18.02.2021::22:18", "18.02.2021::22:22");
+                "18.07.2021::21:18", "18.07.2021::21:19");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("castable", spell.castable);
+        jsonObject.put("coolDown", spell.coolDown);
+        jsonObject.put("fireball", jsonFBall);
+        jsonObject.put("log", jsonLog);
+
+        //JSON Obj to Console
+        System.out.println(jsonObject);
+        System.out.println(new JSONObject(spell));
 
         //object to JSON string
         final Gson gson = new GsonBuilder().create();
