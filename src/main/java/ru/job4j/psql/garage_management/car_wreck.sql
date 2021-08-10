011c4706fa124aafe4850bd9df1fdd9d15be9b9d
i1rr <i1r@tuta.io>
2021-08-10 21:49:24 +1000

    car_wrecking.sql

--- src/main/java/ru/job4j/psql/garage_management/car_wreck.sql
+++ src/main/java/ru/job4j/psql/garage_management/car_wreck.sql
@@ -1,77 +1,82 @@
--- Vehicle table
-create table vehicle (
-	id serial primary key,
-	name varchar(50)
-);
-
--- Vehicle body table
+-- Vehicle body
 create table body (
 	id serial primary key,
-	color varchar(30),
-	vehicle_id int references vehicle(id)
+	color varchar(30)
 );
 
--- Vehicle engine table
+-- Vehicle engine
 create table engine (
 	id serial primary key,
-	volume varchar (20),
-	vehicle_id int references vehicle(id)
+	volume varchar (20)
 );
 
--- Vehicle gearbox table
+-- Vehicle gearbox
 create table gearbox (
 	id serial primary key,
-	type varchar(10),
-	vehicle_id int references vehicle(id)
+	type varchar(10)
 );
 
--- Inserting 3 cars
-insert into vehicle (name) values 
-	('Toyota Land Cruiser'),
-	('BMW X6'),
-	('VAZ 2109');
+-- Vehicle itself
+create table vehicle (
+	id serial primary key,
+	name varchar(50),
+	body_id int references body(id),
+	engine_id int references engine(id),
+	gearbox_id int references gearbox(id)
+);
 
 -- Inserting 5 vehicle bodies
-insert into body (color, vehicle_id) values
-	('Marble white', 1),
-	('Imola Red', 2),
-	('Yellow', 3),
-	('Golden', null),
-	('Black', null);
+insert into body (color) values
+	('Marble white'),
+	('Imola Red'),
+	('Yellow'),
+	('Golden'),
+	('Black');
 	
 -- Inserting 5 car engines	
-insert into engine (volume, vehicle_id) values
-	('4.5L v8', 1),
-	('4.4L v8', 2),
-	('1.3L v4', 3),
-	('2.0L v4', null),
-	('3.0L v6', null);
+insert into engine (volume) values
+	('4.5L v8'),
+	('4.4L v8'),
+	('1.3L v4'),
+	('2.0L v4'),
+	('3.0L v6');
 
 -- Inserting 5 car gearboxes
-insert into gearbox (type, vehicle_id) values
-	('6s Auto', 1),
-	('8s Auto', 2),
-	('4s Manual', 3),
-	('5s Auto', null),
-	('7s Auto', null);
-
+insert into gearbox (type) values
+	('6s Auto'),
+	('8s Auto'),
+	('4s Manual'),
+	('5s Auto'),
+	('7s Auto');
+	
+-- Inserting 3 cars
+insert into vehicle (name, body_id, engine_id, gearbox_id) values 
+	('Toyota Land Cruiser', 1, 1, 1),
+	('BMW X6', 2, 2, 2),
+	('VAZ 2109', 3, 3, 3);
+	
 -- 1) Вывести список всех машин и все привязанные к ним детали
-select vehicle.name as Car, body.color as Color, engine.volume as Engine, gearbox.type as Gearbox
-from vehicle, body, engine, gearbox
-where (vehicle.id = gearbox.vehicle_id)
-and (vehicle.id = body.vehicle_id)
-and (vehicle.id = engine.vehicle_id);
+select v.name as Car, b.color as Color, e.volume as Engine, g.type as Gearbox
+from vehicle as v, body as b, engine as e, gearbox as g
+where b.id = v.body_id
+and e.id = v.engine_id
+and g.id = v.gearbox_id;
 
 -- 2) Вывести отдельно детали, которые не используются НИ в одной машине, кузова, двигатели, коробки передач.
-select body.color as "body color"
-from body
-where (body.vehicle_id is null);
+select b.color as "Stock body"
+from vehicle as v
+right join body as b
+on b.id = v.body_id
+where v.body_id is null;
 
-select engine.volume as "engine"
-from engine
-where (engine.vehicle_id is null);
+select e.volume as "Stock engine"
+from vehicle as v
+right join engine as e
+on e.id = v.engine_id
+where v.engine_id is null;
 
-select gearbox.type as "gearbox"
-from gearbox
-where (gearbox.vehicle_id is null);
-
+select g.type as "Stock transmission"
+from vehicle as v
+right join gearbox as g
+on g.id = v.gearbox_id
+where v.gearbox_id is null;
