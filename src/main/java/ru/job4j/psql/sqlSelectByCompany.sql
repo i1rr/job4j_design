d4ad5baa99e217f8607f266aadd9b638db0a4ad4
i1rr <i1r@tuta.io>
2021-09-18 15:37:34 +1000

    SQL Select by company fix

--- src/main/java/ru/job4j/psql/sqlSelectByCompany.sql
+++ src/main/java/ru/job4j/psql/sqlSelectByCompany.sql
@@ -1,20 +1,19 @@
-SELECT
-person.name as Employee,
-company.name as Company
-FROM
-person
-JOIN
-company
-ON
-person.company_id = company.id
-WHERE
-company_id != 5
-ORDER BY
-company.name;
+ SELECT
+ p.name as Employee,
+ c.name as Company
+ FROM
+ person p, company c
+ WHERE
+ p.company_id = c.id
+ and
+ p.company_id != 5
+ ORDER BY
+ c.name;
 
--- Надеюсь это говнище не увидит потенциальный работодатель
-SELECT MAX(c.name), MAX(ppl.num)
-FROM
-(SELECT p.company_id as pid, COUNT(p.name) as num FROM company c JOIN person p ON c.id = p.company_id GROUP BY pid) as ppl
-JOIN company c
-ON c.id = ppl.pid;
+
+SELECT c.name as Company, COUNT(p.name) as Amount
+FROM company c, person p
+WHERE c.id = p.company_id
+GROUP BY c.name
+ORDER BY Amount desc
+LIMIT 1;
