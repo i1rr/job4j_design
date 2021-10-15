021fd3f06b27df77718bbb4379edbbf862b98acd
i1rr <i1r@tuta.io>
2021-10-15 14:14:03 +1100

    Test Driven Development

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.tdd;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.OCTOBER, 21, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test(expected = NoAvailableTicketsException.class)
+    public void whenNoAvailableTicketsLeft() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, Calendar.OCTOBER, 21, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void whenSessionDoesNotExist() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertNull(sessions);
+    }
+
+}
