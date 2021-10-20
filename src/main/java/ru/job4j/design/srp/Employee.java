565111388abaaccae1d2627c7e7708c1f370c526
i1rr <i1r@tuta.io>
2021-10-20 17:44:00 +1100

    Reports for everyone (checkStyle fix)

--- src/main/java/ru/job4j/design/srp/Employee.java
+++ src/main/java/ru/job4j/design/srp/Employee.java
@@ -50,8 +50,13 @@ public class Employee {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Employee employee = (Employee) o;
         return Objects.equals(name, employee.name);
     }
